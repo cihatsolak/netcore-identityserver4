8f9ce16b228618dcabe4dfaaf55a91196ff82630
Cihat Solak <cihatsolak@hotmail.com>
2021-06-09 23:06:55 +0300

    Added custom user repository

---
+++ NetCoreIdentityServer/IdentityServer.AuthServer/Repositories/Users/ICustomUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using IdentityServer.AuthServer.Models;
+using System.Threading.Tasks;
+
+namespace IdentityServer.AuthServer.Repositories.Users
+{
+    internal interface ICustomUserRepository
+    {
+        Task<bool> ValidatePasswordAsync(string email, string password);
+        Task<CustomUser> FindByIdAsync(int id);
+        Task<CustomUser> FindByEmailAsync(string email);
+    }
+}
